9fcc18ee799aaf1b3b3ff8f7157f0bcdecee780c
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2024-03-10 18:38:28 +0300

    entity package has been created. Created role and user interfaces for auth-service

---
+++ auth-service/src/main/java/com/project/authservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.authservice.repository;
+
+import com.project.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("select u from User u left JOIN FETCH u.roles where u.username = ?1")
+    Optional<User> findByUsernameEagerly(String username);
+}
