6ffcf26c6c1052e426ad8a2e8bfab24b68abe027
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2024-03-11 16:12:39 +0300

    JWTTokenGeneratorFilter created

---
+++ auth-service/src/main/java/com/project/authservice/filter/JWTTokenGeneratorFilter.java
@@ -0,0 +1,59 @@
+package com.project.authservice.filter;
+
+import com.project.authservice.constants.ApplicationConstants;
+import com.project.authservice.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static com.project.authservice.constants.SecurityConstants.*;
+
+public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        User user = (User) principal;
+        SecretKey key = Keys.hmacShaKeyFor(JWT_KEY.getBytes(StandardCharsets.UTF_8));
+        String jwt = Jwts.builder()
+                .setIssuer(ISSUER)
+                .setSubject(SUBJECT)
+                .claim(CLAIM_USERNAME, authentication.getName())
+                .claim(CLAIM_ID, user.getId())
+                .claim(CLAIM_ROLE, authentication.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.joining(",")))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 300000000))
+                .signWith(key)
+                .compact();
+        response.setHeader(AUTH_HEADER, jwt);
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return !ApplicationConstants.LOGIN.equals(request.getServletPath());
+    }
+
+
+}
