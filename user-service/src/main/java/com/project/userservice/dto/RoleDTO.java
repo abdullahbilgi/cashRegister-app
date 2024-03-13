691d5930452118befbb1cffaae443226636f43e7
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2024-03-13 17:12:12 +0300

    DTO classes created for user-service project

---
+++ user-service/src/main/java/com/project/userservice/dto/RoleDTO.java
@@ -0,0 +1,23 @@
+package com.project.userservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RoleDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Role name is required")
+    @Size(min = 2, max = 50, message = "Role name must be between 2 and 50 characters")
+    private String name;
+
+    @Size(max = 250, message = "Role description should not exceed 250 characters")
+    private String description;
+
+}
---
+++ user-service/src/main/java/com/project/userservice/dto/UserDTO.java
@@ -0,0 +1,40 @@
+package com.project.userservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    @Pattern(regexp = "^[A-Za-z]+$", message = "Name must contain only letters")
+    private String name;
+
+    @NotBlank(message = "Surname is required")
+    @Size(min = 2, max = 100, message = "Surname must be between 2 and 100 characters")
+    @Pattern(regexp = "^[A-Za-z]+$", message = "Surname must contain only letters")
+    private String surname;
+
+    private Set<UserDTO> roles;
+}
