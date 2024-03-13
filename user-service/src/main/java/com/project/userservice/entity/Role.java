b6dc5c91e0cd557817fddf269a97c94b5749fde5
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2024-03-13 16:50:48 +0300

    Entity classes created for user-service project

---
+++ user-service/src/main/java/com/project/userservice/entity/Role.java
@@ -0,0 +1,30 @@
+package com.project.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "_role")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "_name", unique = true)
+    private String name;
+
+    @Column(nullable = false, name = "_description")
+    private String description;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Set<User> users = new HashSet<>();
+}
