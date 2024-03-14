bfcd5f078e73ecdfebac880fb2380848344cb635
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2024-03-14 17:35:45 +0300

    Services interfaces and Services classes created for user-service project

---
+++ user-service/src/main/java/com/project/userservice/service/RoleService.java
@@ -0,0 +1,18 @@
+package com.project.userservice.service;
+
+import com.project.userservice.entity.Role;
+
+import java.util.List;
+
+public interface RoleService {
+
+    List<Role> findAllRoles();
+
+    Role findRole(Long id);
+
+    Role saveRole(Role role);
+
+    void deleteRole(Long id);
+
+
+}
