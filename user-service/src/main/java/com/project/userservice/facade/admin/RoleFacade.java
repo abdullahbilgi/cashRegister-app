dc73d4af1f0662f3370d347849dfd917bcc82146
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2024-03-15 15:26:11 +0300

    By keeping the RoleService simple, the necessary operations were done on the role facade for user-service project

---
+++ user-service/src/main/java/com/project/userservice/facade/admin/RoleFacade.java
@@ -0,0 +1,59 @@
+package com.project.userservice.facade.admin;
+
+import com.project.userservice.dto.RoleDTO;
+import com.project.userservice.entity.Role;
+import com.project.userservice.mapper.RoleMapper;
+import com.project.userservice.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class RoleFacade {
+
+    private final RoleService roleService;
+    private final RoleMapper roleMapper;
+
+    public List<RoleDTO> findAllRoles(){
+        return roleService.findAllRoles()
+                .stream()
+                .map(roleMapper::map)
+                .toList();
+    }
+
+    public RoleDTO findRole(Long id){
+        Role role = roleService.findRole(id);
+
+        return roleMapper.map(role);
+
+    }
+
+    public RoleDTO saveRole(RoleDTO roleDTO){
+        Role role = roleMapper.mapDto(roleDTO);
+
+        return saveAndMap(role);
+    }
+
+    public RoleDTO updateRole(Long id, RoleDTO roleDTO){
+        findRole(id);// control if it exists
+        Role savingRole = roleMapper.mapDto(roleDTO);
+        savingRole.setId(id);
+
+        return saveAndMap(savingRole);
+    }
+
+    public void deleteRole(Long id){
+        roleService.deleteRole(id);
+    }
+
+    private RoleDTO saveAndMap(Role role){
+        Role savedRole = roleService.saveRole(role);
+
+        return roleMapper.map(savedRole);
+    }
+
+
+
+}
