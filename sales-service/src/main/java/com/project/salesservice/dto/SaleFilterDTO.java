28d175d5a0878668488c8aa6546a3c8183f84cbf
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2024-06-24 01:17:56 +0300

    dto classes created for sales-service

---
+++ sales-service/src/main/java/com/project/salesservice/dto/SaleFilterDTO.java
@@ -0,0 +1,20 @@
+package com.project.salesservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleFilterDTO {
+
+    private Long minCount;
+    private Long maxCount;
+    private Double minTotalPrice;
+    private Double maxTotalPrice;
+    private Long productId;
+    private Long userId;
+    private Integer page = 1;
+    private Integer size = 10;
+}
---
+++ sales-service/src/main/java/com/project/salesservice/dto/saleDTO.java
@@ -0,0 +1,40 @@
+package com.project.salesservice.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@Builder
+public class saleDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Ticket code is mandatory")
+    @Size(min = 5, max = 20, message = "Sale code must be between 5 and 20 characters")
+    private String code;
+
+    @NotNull(message = "Ticket count is mandatory")
+    @Min(value = 1, message = "Ticket count must be at least 1")
+    private Integer count;
+
+    @NotNull(message = "Total price is mandatory")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Total price must be greater than 0")
+    private Double totalPrice;
+
+    @NotNull(message = "User ID is mandatory")
+    private Long userId;
+
+    @NotNull(message = "Product ID is mandatory")
+    private Long eventId;
+
+    private boolean isSuspended;
+
+    @NotNull(message = "Sale date is mandatory")
+    @Future(message = "Sale date must be in the future")
+    private Date saleDate;
+
+}
