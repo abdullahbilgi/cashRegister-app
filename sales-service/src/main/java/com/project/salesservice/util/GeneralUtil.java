e6502a9b4dec2190d5a5f0fe6c97fdbf56b438de
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2024-06-24 22:57:37 +0300

    Util class added sales-service

---
+++ sales-service/src/main/java/com/project/salesservice/util/GeneralUtil.java
@@ -0,0 +1,33 @@
+package com.project.salesservice.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Objects;
+
+import static com.project.salesservice.constants.SecurityConstants.CLAIM_ID;
+
+public class GeneralUtil {
+
+    public static Long getUserIdFromRequest(){
+        HttpServletRequest request = getRequest();
+        String idStr = String.valueOf(request.getAttribute(CLAIM_ID));
+        return Long.valueOf(idStr);
+    }
+
+    public static String getAttributeFromRequest(String key){
+        HttpServletRequest request = getRequest();
+        Object attribute = request.getAttribute(key);
+        if (Objects.isNull(attribute)){
+            throw new RuntimeException("There's no attribute named: " + key);
+        }
+
+        return String.valueOf(attribute);
+    }
+
+    public static HttpServletRequest getRequest(){
+        return ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+    }
+
+}
